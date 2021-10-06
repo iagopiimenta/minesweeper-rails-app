9aeede2c3c846e690d35b01f867db8da23ecc3ca
Iago <iagopimenta.s@gmail.com>
2021-10-05 21:38:40 -0300

    Add default tiles value

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,8 @@
 class Game < ApplicationRecord
   enum state: [ :pending_mining, :playing, :game_over ], _prefix: true, _default: :pending_mining
 
+  attribute :tiles, default: -> { {} }
+
   before_save :change_state, if: :state_pending_mining?
 
   private
